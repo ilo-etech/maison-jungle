18b2580e729614af73af61b36d6dce1531590a09
ilo-etech <m.rasatandraina@etechconsulting-mg.com>
2023-08-24 23:32:40 +0300

    [ADD] Add Component Banner and Cart

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from './Banner';
+import Cart from './Cart';
+import { Component } from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Banner />
+      <Cart />
+    </React.Fragment>
+
+  );
+}
+
+export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,18 @@
+function Cart() {
+    const monstera_price = 8.0;
+    const lierre_price = 10.0;
+    const bouquet_fleurs_price = 15.0;
+    return (
+        <div>
+            <h2>Panier</h2>
+            <ul>
+                <li>Monstera : {monstera_price} €</li>
+                <li>Lierre : {lierre_price} €</li>
+                <li>Bouquet de fleurs : {bouquet_fleurs_price} €</li>
+            </ul>
+            Total : {monstera_price + lierre_price + bouquet_fleurs_price} €
+        </div>
+    );
+}
+
+export default Cart
